dd13b65d3718fbce6437f8638bb502fb15c2dca1
Grisham Balloo <grisham98@windowslive.com>
2026-04-19 16:20:11 +1200

    fix(hal): Fixing Hardfault
    -Realised that the project was configured for the wrong chip "stm32f100c6t6", even though on the chip itself it is labled "stm32f100c8t6".There is 128kb of flash memory and 20 kb of sram, which would mean it's most likely a "stm32f100cBt6" chip incorrectly labelled
    - Discovered the fault with register inspection in Ozone, where the system jumped to address zero due to null-pointers in the Low-Density vector table.
    - Migrated to the correct 'xB' startup code and linker script to properly map the 20KB SRAM and peripheral interrupts.
    - Verified fix with successful Feetech servo communication and movement.

--- App/Src/app_main.c
+++ App/Src/app_main.c
@@ -1,23 +1,29 @@
 /*
-Application Test Code for STM32F104C8T6 Dev Board Using UART 2 (UART 1 damaged, verified with oscilsicope) + Waveshare Bus Servo Adapter
+Application Test Code for STM32F104CBT6 Dev Board Using UART 2 (UART 1 damaged, verified with oscilsicope)
++ Waveshare Bus Servo Adapter
 1000000 Baud rate.
 No paritity Bits
 1 stop bit
 Word Length 8 bits
 Full Duplex
+Note: if not using Wave Share Bus Servo adapter, would need to be in half duplex, because these servo have one wire for both rx adn tx (which the adapter automatically handles)
 */
+
 #include "sts_protocol.h"
 #include "sts_servo.h"
 #include "sts_registers.h"
 #include "sts_servo_cmd.h"
 #include "sts_ports_stm32.h"
-#include "usart.h"
+#include "main.h"
 #include <stdint.h>
+
+extern UART_HandleTypeDef huart2;
 
 
 void AppMain(void) {
-  sts_bus_t servo_bus = {0};
-  sts_servo_t servo_1 = {0};
+  
+static sts_bus_t servo_bus = {0};
+static sts_servo_t servo_1 = {0};
 
   STS_Bus_Init(&servo_bus, &huart2, STM32_UART_Transmit, STM32_UART_Receive);
   STS_Servo_Init(&servo_1, &servo_bus, 2);
@@ -37,6 +43,7 @@ void AppMain(void) {
       HAL_Delay(1000); 
     }
     else
+    
     {
       // Slow blink for failure
         HAL_GPIO_WritePin(GPIOC, GPIO_PIN_13, GPIO_PIN_RESET); 
